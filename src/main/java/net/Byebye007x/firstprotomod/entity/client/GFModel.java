cc623f967535c2064279b39f70a536228ea061e7
NoCODEEE <noonemore.gg@gmail.com>
2024-08-16 04:28:21 +0700

    Working GF

--- src/main/java/net/Byebye007x/firstprotomod/entity/client/GFModel.java
+++ src/main/java/net/Byebye007x/firstprotomod/entity/client/GFModel.java
@@ -4,23 +4,35 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.Byebye007x.firstprotomod.entity.animations.ModAnimationDefinitionsGf;
 import net.Byebye007x.firstprotomod.entity.custom.GFEntity;
-import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import org.joml.Vector3f;
 
-public class GFModel<T extends Entity> extends HierarchicalModel<T> {
-		private final ModelPart gf;
-		private final ModelPart head;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-	public GFModel(ModelPart root) {
-			this.gf = root.getChild("gf");
-			this.head = gf.getChild("head");
-		}
+public class GFModel<T extends Entity> extends HumanoidModel<LivingEntity> {
+	private static final Vector3f ANIMATION_VECTOR_CACHE = new Vector3f();
+	private final ModelPart gf;
+	private final ModelPart head;
 
-		public static LayerDefinition createGFBodyLayer() {
+	public GFModel(ModelPart pRoot) {
+		super(pRoot);
+		this.gf = pRoot.getChild("gf");
+		this.head = gf.getChild("head");
+	}
+
+	public static LayerDefinition createGFBodyLayer() {
 			MeshDefinition meshdefinition = new MeshDefinition();
 			PartDefinition partdefinition = meshdefinition.getRoot();
 
@@ -47,12 +59,18 @@ public class GFModel<T extends Entity> extends HierarchicalModel<T> {
 			return LayerDefinition.create(meshdefinition, 64, 64);
 		}
 
-	@Override
-	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-		this.root().getAllParts().forEach(ModelPart::resetPose);
-		this.applyHeadRotation(netHeadYaw, headPitch, ageInTicks);
 
-		this.animateWalk(ModAnimationDefinitionsGf.GF_WALKING, limbSwing, limbSwingAmount, 2f, 2.5f);
+//	this.animateWalk(ModAnimationDefinitionsGf.GF_WALKING, limbSwing, limbSwingAmount, 2f, 2.5f);
+//		this.animate(((GFEntity) entity).attackAnimationState, ModAnimationDefinitionsGf.GF_SWING, ageInTicks, 1f);
+//		this.animate(((GFEntity) entity).idleAnimationState, ModAnimationDefinitionsGf.GF_IDLE, ageInTicks, 1f);
+
+
+	@Override
+	public void setupAnim(LivingEntity pEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
+		this.root().getAllParts().forEach(ModelPart::resetPose);
+		this.applyHeadRotation(pNetHeadYaw, pHeadPitch, pAgeInTicks);
+
+		this.animated(((GFEntity) pEntity).idleAnimationState, ModAnimationDefinitionsGf.GF_IDLE, pAgeInTicks);
 	}
 
 	private void applyHeadRotation(float pNetHeadYaw, float pHeadPitch, float pAgeInTicks) {
@@ -63,13 +81,64 @@ public class GFModel<T extends Entity> extends HierarchicalModel<T> {
 		this.head.xRot = pHeadPitch * ((float)Math.PI / 180F);
 	}
 
-		@Override
+	protected void animated(AnimationState pAnimationState, AnimationDefinition pAnimationDefinition, float pAgeInTicks) {
+		animating(pAnimationState, pAnimationDefinition, pAgeInTicks, 1.0F);
+	}
+
+	protected void animating(AnimationState pAnimationState, AnimationDefinition pAnimationDefinition, float pAgeInTicks, float pSpeed) {
+		pAnimationState.updateTime(pAgeInTicks, pSpeed);
+		pAnimationState.ifStarted((p_233392_) -> {
+			animate(this, pAnimationDefinition, p_233392_.getAccumulatedTime(), 1.0F, ANIMATION_VECTOR_CACHE);
+		});
+	}
+
+
+	public static void animate(GFModel<?> pModel, AnimationDefinition pAnimationDefinition, long pAccumulatedTime, float pScale, Vector3f pAnimationVecCache) {
+		float f = getElapsedSeconds(pAnimationDefinition, pAccumulatedTime);
+
+		for(Map.Entry<String, List<AnimationChannel>> entry : pAnimationDefinition.boneAnimations().entrySet()) {
+			Optional<ModelPart> optional = pModel.getAnyDescendantWithName(entry.getKey());
+			List<AnimationChannel> list = entry.getValue();
+			optional.ifPresent((p_232330_) -> {
+				list.forEach((p_288241_) -> {
+					Keyframe[] akeyframe = p_288241_.keyframes();
+					int i = Math.max(0, Mth.binarySearch(0, akeyframe.length, (p_232315_) -> {
+						return f <= akeyframe[p_232315_].timestamp();
+					}) - 1);
+					int j = Math.min(akeyframe.length - 1, i + 1);
+					Keyframe keyframe = akeyframe[i];
+					Keyframe keyframe1 = akeyframe[j];
+					float f1 = f - keyframe.timestamp();
+					float f2;
+					if (j != i) {
+						f2 = Mth.clamp(f1 / (keyframe1.timestamp() - keyframe.timestamp()), 0.0F, 1.0F);
+					} else {
+						f2 = 0.0F;
+					}
+
+					keyframe1.interpolation().apply(pAnimationVecCache, f2, akeyframe, i, j, pScale);
+					p_288241_.target().apply(p_232330_, pAnimationVecCache);
+				});
+			});
+		}
+
+	}
+	private static float getElapsedSeconds(AnimationDefinition pAnimationDefinition, long pAccumulatedTime) {
+		float f = (float)pAccumulatedTime / 1000.0F;
+		return pAnimationDefinition.looping() ? f % pAnimationDefinition.lengthInSeconds() : f;
+	}
+
+	public Optional<ModelPart> getAnyDescendantWithName(String pName) {
+		return pName.equals("root") ? Optional.of(this.root()) : this.root().getAllParts().filter((p_233400_) -> p_233400_.hasChild(pName)).findFirst().map((p_233397_) -> p_233397_.getChild(pName));
+	}
+
+    public ModelPart root() {
+        return gf;
+    }
+
+    @Override
 		public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
 			gf.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		}
 
-	@Override
-	public ModelPart root() {
-		return gf;
-	}
 }
--- src/main/java/net/Byebye007x/firstprotomod/entity/custom/GFEntity.java
+++ src/main/java/net/Byebye007x/firstprotomod/entity/custom/GFEntity.java
@@ -1,84 +1,122 @@
 package net.Byebye007x.firstprotomod.entity.custom;
 
 import net.Byebye007x.firstprotomod.entity.ModEntities;
+import net.Byebye007x.firstprotomod.entity.ai.GfAttackGoal;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public class GFEntity extends Animal {
-    private static final EntityDataAccessor<Integer> DATA_REMAINING_ANGER_TIME = SynchedEntityData.defineId(GFEntity.class, EntityDataSerializers.INT);
-    private UUID persistentAngerTarget;
+public class GFEntity extends TamableAnimal {
+    private static final EntityDataAccessor<Boolean> ATTACKING_GF =
+            SynchedEntityData.defineId(GFEntity.class, EntityDataSerializers.BOOLEAN);
+    protected static final EntityDataAccessor<Byte> DATA_FLAGS_ID =
+            SynchedEntityData.defineId(GFEntity.class, EntityDataSerializers.BYTE);
+    protected static final EntityDataAccessor<Optional<UUID>> GF_IS_MINE =
+            SynchedEntityData.defineId(GFEntity.class, EntityDataSerializers.OPTIONAL_UUID);
 
-    public GFEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
+    public GFEntity(EntityType<? extends TamableAnimal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
-//        this.setTame(true);
+        this.setTame(false);
     }
 
-    public final AnimationState idleAnimationState = new AnimationState();
-    private int idleAnimationTimeout = 0;
+//    public final AnimationState idleAnimationState = new AnimationState();
+//    private int idleAnimationTimeout = 0;
+//    public final AnimationState attackAnimationState = new AnimationState();
+//    public int attackAnimationTimeout = 0;
+
+
+//    @Override
+//    public void tick() {
+//        super.tick();
+//
+//        if(this.level().isClientSide()) {
+//            setupAnimationStates();
+//        }
+//    }
+//    private void setupAnimationStates() {
+//        if (isInSittingPose()) {
+////            if  (this.idleAnimationTimeout <= 0) {
+////                this.idleAnimationTimeout = this.random.nextInt(40) + 80;
+////                this.idleAnimationState.start(this.tickCount);
+////            } else {
+////                --this.idleAnimationTimeout;
+////                }
+//            System.out.print('A');
+//        }
+//        if (this.isAttacking() && attackAnimationTimeout <= 0) {
+//            attackAnimationTimeout = 6;
+//            attackAnimationState.start(this.tickCount);
+//        } else {
+//            --this.attackAnimationTimeout;
+//        }
+//
+//        if (!this.isAttacking()) {
+//            attackAnimationState.stop();
+//        }
+//    }
+//
+//    @Override
+//    protected void updateWalkAnimation(float pPartialTick) {
+//        float f;
+//        if(this.getPose() == Pose.STANDING) {
+//            f = Math.min(pPartialTick * 6F, 1f);
+//        } else {
+//            f = 0f;
+//        }
+//
+//        this.walkAnimation.update(f, 0.2f);
+//    }
+
+    public void setAttacking(boolean attacking) {
+        this.entityData.set(ATTACKING_GF, attacking);
+    }
+
+    public boolean isAttacking() {
+        return this.entityData.get(ATTACKING_GF);
+    }
 
     @Override
-    public void tick() {
-        super.tick();
-
-        if(this.level().isClientSide()) {
-            setupAnimationStates();
-        }
-    }
-
-    private void setupAnimationStates() {
-        if(this.idleAnimationTimeout <= 0) {
-            this.idleAnimationTimeout = this.random.nextInt(40) + 80;
-            this.idleAnimationState.start(this.tickCount);
-        } else {
-            --this.idleAnimationTimeout;
-        }
-    }
-
-    @Override
-    protected void updateWalkAnimation(float pPartialTick) {
-        float f;
-        if(this.getPose() == Pose.STANDING) {
-            f = Math.min(pPartialTick * 6F, 1f);
-        } else {
-            f = 0f;
-        }
-
-        this.walkAnimation.update(f, 0.2f);
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(ATTACKING_GF, false);
+        this.entityData.define(DATA_FLAGS_ID, (byte)0);
+        this.entityData.define(GF_IS_MINE, Optional.empty());
     }
 
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, true));
-//        this.goalSelector.addGoal(2, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
-        this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 1.0D, true));
-        this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.15d));
-        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class,3f));
-        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
-//        this.targetSelector.addGoal(0, new OwnerHurtByTargetGoal(this));
-//        this.targetSelector.addGoal(1, new OwnerHurtTargetGoal(this));
-//        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isAngryAt));
+        this.goalSelector.addGoal(1, new SitWhenOrderedToGoal(this));
+        this.goalSelector.addGoal(3, new GfAttackGoal(this, 2.0d, true));
+        this.goalSelector.addGoal(2, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+        this.goalSelector.addGoal(3, new WaterAvoidingRandomStrollGoal(this, 1.15d));
+        this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Player.class,3f));
+        this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+
+        this.targetSelector.addGoal(0, new OwnerHurtByTargetGoal(this));
+        this.targetSelector.addGoal(1, new OwnerHurtTargetGoal(this));
+        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
@@ -101,31 +139,78 @@ public class GFEntity extends Animal {
         return item.isEdible() && !pStack.is(Items.ROTTEN_FLESH) ;
     }
 
+    @Override
+    public InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        Item item = itemstack.getItem();
+        if (this.level().isClientSide) {
+            boolean flag = this.isOwnedBy(pPlayer) || this.isTame() || itemstack.is(Items.DIAMOND) && !this.isTame() && !this.isAttacking();
+            return flag ? InteractionResult.CONSUME : InteractionResult.PASS;
+        } else if (this.isTame()) {
+            if (this.isFood(itemstack) && this.getHealth() < this.getMaxHealth()) {
+                this.heal((float)itemstack.getFoodProperties(this).getNutrition());
+                if (!pPlayer.getAbilities().instabuild) {
+                    itemstack.shrink(1);
+                }
 
+                this.gameEvent(GameEvent.EAT, this);
+                return InteractionResult.SUCCESS;
+            } else {
+                InteractionResult interactionresult = super.mobInteract(pPlayer, pHand);
+                if ((!interactionresult.consumesAction() || this.isBaby()) && this.isOwnedBy(pPlayer)) {
+                    this.setOrderedToSit(!this.isOrderedToSit());
+                    this.jumping = false;
+                    this.navigation.stop();
+                    this.setTarget((LivingEntity)null);
+                    return InteractionResult.SUCCESS;
+                } else {
+                    return interactionresult;
+                }
+            }
+        }
+         else if (itemstack.is(Items.DIAMOND) && !this.isAttacking()) {
+            if (!pPlayer.getAbilities().instabuild) {
+                itemstack.shrink(1);
+            }
 
-    //    @Override
-//    public int getRemainingPersistentAngerTime() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public void setRemainingPersistentAngerTime(int pRemainingPersistentAngerTime) {
-//        this.entityData.set(DATA_REMAINING_ANGER_TIME, pRemainingPersistentAngerTime);
-//    }
-//
-//    @Nullable
-//    @Override
-//    public UUID getPersistentAngerTarget() {
-//        return this.persistentAngerTarget;
-//    }
-//
-//    @Override
-//    public void setPersistentAngerTarget(@Nullable UUID pPersistentAngerTarget) {
-//
-//    }
-//
-//    @Override
-//    public void startPersistentAngerTimer() {
-//
-//    }
+            if (this.random.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, pPlayer)) {
+                this.tame(pPlayer);
+                this.navigation.stop();
+                this.setTarget((LivingEntity)null);
+                this.setOrderedToSit(true);
+                this.level().broadcastEntityEvent(this, (byte)7);
+            } else {
+                this.level().broadcastEntityEvent(this, (byte)6);
+            }
+
+            return InteractionResult.SUCCESS;
+        } else {
+            return super.mobInteract(pPlayer, pHand);
+        }
+    }
+
+    @Override
+    public InteractionHand getUsedItemHand() {
+        return InteractionHand.MAIN_HAND; // Main hand or offhand
+    }
+
+    // Method to set the held item
+    public void setHeldItem(ItemStack stack) {
+        this.setItemInHand(InteractionHand.MAIN_HAND, stack); // Set item in main hand
+    }
+
+    // Method to get the held item
+    public ItemStack getHeldItem() {
+        return this.getItemInHand(InteractionHand.MAIN_HAND);
+    }
+
+    // Ensure the item is visually rendered in the entity's hand
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        if (!this.level().isClientSide && this.tickCount % 20 == 0) {
+            this.setHeldItem(new ItemStack(Items.IRON_SWORD));
+        }
+    }
+
 }
